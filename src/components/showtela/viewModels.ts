d775c7a93dcb84fbf86579eb7ad45ab29e353169
jonpearlandpig <jonathan@pearlandpig.com>
2026-05-19 09:09:03 -0500

    Remove orphaned viewModels and normalizeCrusadeData files

--- src/components/showtela/viewModels.ts
+++
@@ -1,7 +0,0 @@
-import type { FeedItem, ShowTelaViewModel } from './types';
-
-export type RawCrusadeData = {
-  feed: FeedItem[];
-};
-
-export type NormalizeFn = (data: RawCrusadeData) => ShowTelaViewModel;
